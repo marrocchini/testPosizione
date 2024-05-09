fb0028f44a933d998e442cdb56f9d87564beb7ee
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-05-09 10:15:47 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/actions/main_action.dart
+++ lib/custom_code/actions/main_action.dart
@@ -6,10 +6,11 @@ import 'package:flutter/material.dart';
 // Begin custom action code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
 
+import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/scheduler.dart';
 
 import 'dart:async';
-
 import 'dart:io';
 import 'dart:convert';
 
@@ -18,7 +19,6 @@ import 'dart:convert';
 import 'dart:isolate';
 import 'dart:ui';
 import 'package:flutter/gestures.dart';
-
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -26,42 +26,120 @@ import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_background_service_android/flutter_background_service_android.dart';
 import 'package:flutter_background_service_ios/flutter_background_service_ios.dart';
-import 'package:background_fetch/background_fetch.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 Future mainAction() async {
-  // Step 1:  Configure BackgroundFetch as usual.
-  int status = await BackgroundFetch.configure(BackgroundFetchConfig(
-    minimumFetchInterval: 15
-  ), (String taskId) async {  // <-- Event callback.
-    // This is the fetch-event callback.
-    print("[BackgroundFetch] taskId: $taskId");
-  
-    // Use a switch statement to route task-handling.
-    switch (taskId) {
-      case 'com.transistorsoft.customtask':
+  await initializeService();
+}
+
+Future<void> initializeService() async {
+  final service = FlutterBackgroundService();
+
+  const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    "notificationChannelId", // id
+    'MY FOREGROUND SERVICE', // title
+    description:
+        'This channel is used for important notifications.', // description
+    importance: Importance.low, // importance must be at low or higher level
+  );
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  await service.configure(
+      androidConfiguration: AndroidConfiguration(
+        // this will be executed when app is in foreground or background in separated isolate
+        onStart: onStart,
+
+        // auto start service
+        autoStart: true,
+        isForegroundMode: true,
+
+        notificationChannelId:
+            "notificationChannelId", // this must match with notification channel you created above.
+        initialNotificationTitle: 'AWESOME SERVICE',
+        initialNotificationContent: 'Initializing',
+        foregroundServiceNotificationId: 1,
+      ),
+      iosConfiguration: IosConfiguration(
+        autoStart: true,
+        onForeground: onStart,
+        onBackground: onIosBackground,
+      ));
+}
+
+@pragma('vm:entry-point')
+Future<bool> onIosBackground(ServiceInstance service) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  DartPluginRegistrant.ensureInitialized();
+
+  return true;
+}
+
+@pragma('vm:entry-point')
+Future<void> onStart(ServiceInstance service) async {
+  //WidgetsFlutterBinding.ensureInitialized();
+  DartPluginRegistrant.ensureInitialized();
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  List<String> positions = [];
+  bool controllo = true;
+  while (controllo) {
+    gyroscopeEvents.listen(
+      (GyroscopeEvent event) async {
         final now = DateTime.now();
         Position position = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.best);
+            desiredAccuracy: LocationAccuracy.best);
         //Convert Position to String (or any other format suitable for your needs).
         String positionString = "${position.latitude}, ${position.longitude}";
-        await ritornaApiCall([positionString],now.toString()]);
-        break;
-      default:
-        print("Default fetch task");
+        //late AppLifecycleState? _state;
+        //_state = SchedulerBinding.instance.lifecycleState;
+        //positions.add(_state!.name);
+        await ritornaApiCall([positionString, now.toString()]);
+      },
+      onError: (error) async {
+        await ritornaApiCall(["Non hai il giroscopio"]);
+      },
+      cancelOnError: true,
+    );
+    /*await Future.delayed(Duration(seconds: 30));
+    if (service is AndroidServiceInstance) {
+      if (await service.isForegroundService()) {
+        flutterLocalNotificationsPlugin.show(
+          1,
+          'COOL SERVICE',
+          'Awesome ${DateTime.now()}',
+          const NotificationDetails(
+            android: AndroidNotificationDetails(
+              "notificationChannelId",
+              'MY FOREGROUND SERVICE',
+              icon: 'ic_bg_service_small',
+              ongoing: true,
+            ),
+          ),
+        );
+      }
     }
-    // Finish, providing received taskId.
-    BackgroundFetch.finish(taskId);
-  }, (String taskId) async {  // <-- Event timeout callback
-    // This task has exceeded its allowed running-time.  You must stop what you're doing and immediately .finish(taskId)
-    print("[BackgroundFetch] TIMEOUT taskId: $taskId");
-    BackgroundFetch.finish(taskId);
-  });
-  
-  // Step 2:  Schedule a custom "oneshot" task "com.transistorsoft.customtask" to execute 5000ms from now.
-  BackgroundFetch.scheduleTask(TaskConfig(
-    taskId: "com.transistorsoft.customtask",
-    delay: 5000  // <-- milliseconds
-  ));
+    final now = DateTime.now();
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.best);
+    //Convert Position to String (or any other format suitable for your needs).
+    String positionString = "${position.latitude}, ${position.longitude}";
+    //late AppLifecycleState? _state;
+    //_state = SchedulerBinding.instance.lifecycleState;
+    //positions.add(_state!.name);
+    positions.add(now.toString());
+    positions.add(positionString);
+    // Perform the API call with the latest position. Adjust 'ritornaApiCall' to accept position data if needed.
+    if (positions.length % 16 == 0) {
+      await ritornaApiCall(positions);
+    }*/
+  }
 }
 // Set your action name, define your arguments and return parameter,
 // and then add the boilerplate code using the green button on the right!
